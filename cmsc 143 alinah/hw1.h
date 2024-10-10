32786e8ab1b5fb90ea5f4aad023fe157adcc9537
utah3 <144156492+utah3@users.noreply.github.com>
2024-10-10 14:41:17 -0500

    Add files via upload

---
+++ cmsc 143 alinah/hw1.h
@@ -0,0 +1,13 @@
+unsigned char coin_types(unsigned int cents);
+/*takes in a given number of cents and returns the fewest
+number of coins you will need*/
+unsigned short weeks_in_term(unsigned short total_mins,
+unsigned short max_mins_per_lecture,
+unsigned char max_lectures_week);
+/*takes in a given number of minutes you want the class
+to be and returns an extensive */
+char month_ends_in(unsigned char month);
+/*uses switch gives the last letter for the num month*/
+unsigned int lit_segments(unsigned int number);
+/*uses recursive function and two helpers*/
+
